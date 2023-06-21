cc17919c4380071641d8d9d2597833cef276556a
semisenioritis <89476245+semisenioritis@users.noreply.github.com>
2023-06-22 00:04:11 +0530

    Update saver.js

--- saver.js
+++ saver.js
@@ -13,32 +13,35 @@ function youtube_parser(url) {
     else {
       return false;
     } 
-} ;
+} 
 
 function displayer(data, video_title) { 
     var w = window.open('', '', 'width=150,height=100,resizable=no,toolbar=no,top=120,left=1000'); 
     w.document.write("<style>body {font-family: 'Bebas Neue';font-size: 22px;}</style>"); 
+    w.document.write("<style>#prevue--wrapper{ visibility: hidden}</style>"); 
+
     w.document.write("<body>"); 
+    
     w.document.write(video_title); 
-    w.document.write("<br>"); 
-    w.document.write(data.status); 
+
+
     w.document.write("</body>"); 
     setTimeout(function () { w.close(); }, 4000); 
-} ;
+} 
 
-processor=youtube_parser(a);
-const tex_id=String(processor[0]);
-const ttype=String(processor[1]);
+processor=youtube_parser(a)
+const tex_id=String(processor[0])
+const ttype=String(processor[1])
  
 console.log(tex_id);
 
 
 if (ttype == "normal") {
-    var video_title= document.getElementsByClassName("title style-scope ytd-video-primary-info-renderer")[0].innerText;
-};
+    var video_title= String(document.getElementsByClassName("title style-scope ytd-video-primary-info-renderer")[0].innerText).replace("=","");
+}
 else if (ttype == "shorts") {
-    var video_title= document.getElementsByClassName("title style-scope ytd-reel-player-header-renderer")[0].innerText;
-};
+    var video_title= String(document.getElementsByClassName("title style-scope ytd-reel-player-header-renderer")[0].innerText).replace("=","");
+}
 
 
 fetch('###Add your google appscript webapp url here, (for eg. https://script.google.com/macros/s/something_something_and_identifiers_for_the_api_endpoint/exec)###', 
