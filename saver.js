bcef2aaadb16838932043d98f250ffba0f473924
semisenioritis <89476245+semisenioritis@users.noreply.github.com>
2023-06-21 21:17:43 +0530

    Update saver.js

--- saver.js
+++ saver.js
@@ -1,3 +1,5 @@
+// To use this code as a bookmarklet, remove all line breaks and comments in the code and add the comment bellow to the start of the code. After this all code along with the below comment should be on a single line. Also dont forget to replace the dummy google appscript endpoint with your own endpoint.
+// javascript:
 var a = window.location.href; 
 
 function youtube_parser(url) { 
@@ -12,7 +14,7 @@ function youtube_parser(url) {
     else {
       return false;
     } 
-} 
+} ;
 
 function displayer(data, video_title) { 
     var w = window.open('', '', 'width=150,height=100,resizable=no,toolbar=no,top=120,left=1000'); 
@@ -23,27 +25,27 @@ function displayer(data, video_title) {
     w.document.write(data.status); 
     w.document.write("</body>"); 
     setTimeout(function () { w.close(); }, 4000); 
-} 
+} ;
 
-processor=youtube_parser(a)
-const tex_id=String(processor[0])
-const ttype=String(processor[1])
+processor=youtube_parser(a);
+const tex_id=String(processor[0]);
+const ttype=String(processor[1]);
  
 console.log(tex_id);
 
 
 if (ttype == "normal") {
     var video_title= document.getElementsByClassName("title style-scope ytd-video-primary-info-renderer")[0].innerText;
-}
+};
 else if (ttype == "shorts") {
     var video_title= document.getElementsByClassName("title style-scope ytd-reel-player-header-renderer")[0].innerText;
-}
+};
 
 
 fetch('###Add your google appscript webapp url here, (for eg. https://script.google.com/macros/s/something_something_and_identifiers_for_the_api_endpoint/exec)###', 
 { 
     redirect: "follow", method: "POST", body: JSON.stringify({ "videoid": tex_id, "video_title": video_title }), headers: { "Content-Type": "text/plain;charset=utf-8", }, 
-})
+});
 .then((response) => response.json())
 .then((data) => displayer(data, video_title));
 
